35c287838a6a52dcb38912e10eec86d96040e488
philippmoehl <maxxphilipp@gmail.com>
2023-10-21 22:32:57 +0200

    streamlit frontend for torchserve hosting

--- app.py
+++ app.py
@@ -1,29 +1,81 @@
+import logging
+import matplotlib.pyplot as plt
+from PIL import Image
+import requests
 import streamlit as st
 
 from src.model import ImageClassifier
 from src.solution import generate_solution
-from src.utils import setup
+from src.utils import setup_app
+
+INFERENCE_API = "http://localhost:8080"
+MANAGEMENT_API = "http://localhost:8081"
+
+logger = logging.getLogger(__name__)
 
 
-st.header("Predict if plant is healthy")
+def ping_serve():
+    try:
+        res = requests.post(INFERENCE_API + "/ping")
+        status = res.json()["status"]
+        logger.info(f"Ping status: {status}")
+    except Exception as e:
+        logger.error(e)
+        raise ConnectionError("torchserve is not running")
+    
+
+def list_models():
+    try:
+        res = requests.get(MANAGEMENT_API + "/models")
+        models = res.json()["models"]
+        logger.info(f"Available models: {models}")
+        models = {d["modelName"]: d["modelUrl"] for d in models}
+        return models
+    except Exception as e:
+        logger.error(e)
+        raise ConnectionError("torchserve is not running")
+    
+
+def predict(url, file):
+    try:
+        res = requests.post(INFERENCE_API + f"/predictions/{url}", data=file)
+        prediction = res.json()
+        logger.info(prediction)
+        return res.json()
+    except Exception as e:
+        logger.error(e)
+        raise ConnectionError("torchserve is not running")
+    
+
+def show_image(file):
+    st.image(file.getvalue(), caption=file.name)
 
 
 def main():
+    print(open("image.jpg", "rb"))
+    st.header("PLANT DISEASE")
+
+    models = list_models()
+    model_name = st.selectbox(
+        'Choose Model', (name for name, _ in models.items()))
+    
     file_uploaded = st.file_uploader("Choose File", type=["png","jpg","jpeg"])
 
-    if file_uploaded is not None:
-        classifier = ImageClassifier(file_name=file_uploaded)
-        fig = classifier.visalize()
-        prediction, conf_score = classifier.predict()
-
-        st.write(f"{prediction} with a {conf_score} percent confidence."
-)
+    if file_uploaded:
+        show_image(file_uploaded)
+        if st.button("Predict"):
+            out = predict(model_name, file=file_uploaded.getvalue())
+            st.write("Prediction:")
+            st.write(out)
         if st.button("Solution"):
-            solution = generate_solution(prediction)
+            # TODO: link highest conf class to name
+            # solution = generate_solution(out)
+            solution = "Use more water"
             st.write(solution)
-        st.pyplot(fig)
+
 
 
 if __name__ == "__main__":
-    setup()
+    setup_app()
+    ping_serve()
     main()
