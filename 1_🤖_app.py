39ba364e0dd5ef8a2544e93821923335fef1a116
philippmoehl <maxxphilipp@gmail.com>
2023-10-27 17:32:04 +0200

    ready version of frontend/backend integration

--- 1_🤖_app.py
+++ 1_🤖_app.py
@@ -1,104 +1,90 @@
+import argparse
 import datetime
 import logging
 import os
-from PIL import Image
-import requests
 import streamlit as st
 
-from src.solution import generate_solution
-from src.utils import setup_app, get_datetime_stamp
-from src.app_utils import page_config, setup_supabase,  show_image
-
-INFERENCE_API = "http://localhost:8080"
-MANAGEMENT_API = "http://localhost:8081"
+from src.utils import get_datetime_stamp
+from src.app_utils import (
+    page_config, show_image, setup_app
+    )
 
 logger = logging.getLogger(__name__)
 
 
-def store_image(path, image, bucket="plants"):
-    return supabase_client.storage.from_(bucket).upload(
-        path, image, { "content-type": "image/jpeg"})
+@st.cache_resource
+def handle_parse():
+    parser = argparse.ArgumentParser(description="Torchserve Dashboard")
 
-
-def store_prediction(path, cls, conf, id, table="plants"):
-    supabase_client.table(table).insert(
-        {"id": id, "name": path, "label": cls, "confidence": conf}).execute()
-
-
-def ping_serve():
+    parser.add_argument(
+        "--config-file", 
+        default="app_config.yaml", 
+        help="App configuration file."
+    )
     try:
-        res = requests.post(INFERENCE_API + "/ping")
-        status = res.json()["status"]
-        logger.info(f"Ping status: {status}")
-    except Exception as e:
-        logger.warning(e)
-        st.write("torchserve is not running yet, start serving")
-        st.link_button("serve", "http://localhost:8501/serve")
-        st.stop()
-    
-
-def list_models():
-    try:
-        res = requests.get(MANAGEMENT_API + "/models")
-        models = res.json()["models"]
-        logger.info(f"Available models: {models}")
-        models = {d["modelName"]: d["modelUrl"] for d in models}
-        return models
-    except Exception as e:
-        logger.warning(e)
-        raise ConnectionError("torchserve may not running")
-    
-
-def predict(url, file):
-    try:
-        res = requests.post(INFERENCE_API + f"/predictions/{url}", data=file)
-        prediction = res.json()
-        logger.info(prediction)
-        return res.json()
-    except Exception as e:
-        logger.warning(e)
-        raise ConnectionError("torchserve may not running")
+        args = parser.parse_args()
+        return args
+    except SystemExit as e:
+        os._exit(e.code)
 
 
 def main():
-    st.header("PLANT DISEASE")
+    st.title("PLANT DISEASE PREDICTIONS")
+
+    # ----sidebar----
     solution = st.sidebar.toggle("solution")
-    models = list_models()
+    # ---------------
+
+    # check if torch-serve is running
+    if not st.session_state.inference.ping_serve():
+        st.write("torchserve is not running yet, start serving")
+        st.link_button("serve", "http://localhost:8501/serve")
+        st.stop()
+
+    # list available models
+    models = st.session_state.api.get_loaded_models()["models"]
     if models:
+        logger.info(f"Available models: {models}")
+        models_dict = {d["modelName"]: d["modelUrl"] for d in models}
         model_name = st.selectbox(
-            'Choose Model', (name for name, _ in models.items()))
+            'Choose Model', (name for name, _ in models_dict.items()))
     else:
         st.write("torchserve has no registered model yet, register one")
-        st.link_button("serve", "http://localhost:8501/serve")
+        st.link_button(
+            "register", "http://localhost:8501/serve#register-a-model-docs")
         logger.warning("No torchserve model is registered")
         st.stop()
     
     file_uploaded = st.file_uploader("Choose File", type=["png","jpg","jpeg"])
-
     if file_uploaded:
         show_image(file_uploaded.getvalue(), file_uploaded.name)
         if st.button("Predict"):
-            out = predict(model_name, file=file_uploaded.getvalue())
-            st.write("Prediction:")
+            out = st.session_state.inference.predict(model_name, file=file_uploaded.getvalue())
+            if not out:
+                st.write("torchserve may not be running")
+                st.stop()
+            cls, conf = max(out.items(), key=lambda item: item[1])
+            st.markdown("### Prediction")
             st.write(out)
+            
+            # create unique index and file name for buckets
             timestamp = get_datetime_stamp()
             date_format = datetime.datetime.strptime(
                 timestamp, '%Y-%m-%d_%H-%M-%S')
             unix_timestamp = int(datetime.datetime.timestamp(date_format))
-            file = f"{timestamp}.jpg"
-            store_image(file, file_uploaded.getvalue())
-            cls, conf = max(out.items(), key=lambda item: item[1])
-            store_prediction(file, cls, conf, id=unix_timestamp)
+            file_path = f"{timestamp}.jpg"
+
+            st.session_state.supabase.store_image(file_path, file_uploaded.getvalue())
+            st.session_state.supabase.store_prediction(file_path, cls, conf, id=unix_timestamp)
+
             if solution:
-                # TODO: link highest conf class to name
-                # solution = generate_solution(out)
-                solution_text = f"Using more water helps with {cls}"
-                st.write(solution_text)
+                solution = st.session_state.solution.generate_solution(cls)
+                st.markdown("### Solution")
+                st.write(solution)
 
 
 if __name__ == "__main__":
     page_config("App", "🤖")
-    setup_app()
-    ping_serve()
-    supabase_client = setup_supabase()
+    args = handle_parse()
+    setup_app(args.config_file)
     main()
--- pages/3_👉_label.py
+++ pages/3_👉_label.py
@@ -1,82 +1,45 @@
 import logging
-import os
-import supabase # TODO: in utils
 import streamlit as st
-import json
 
-from supabase import create_client
-
-from src.app_utils import auth, handle_status, page_config, setup_supabase
-
+from src.app_utils import auth, handle_status, page_config, show_image
 
 logger = logging.getLogger(__name__)
 
 
-with open("tmp.json", "r") as f:
-    _labels_dict = json.load(f)
-    _labels = [v for k,v in _labels_dict.items()]
-    _labels_to_ints = {
-        v: int(k) for k,v in _labels_dict.items()
-    }
-
-
-def fetch_table(conf, table="plants"):
-    label_subjects = []
-    (_, rows), _ = supabase_client.table(table).select("*").execute()
-    rows = rows
-    for row in rows:
-        if float(row["confidence"]) <= conf:
-            label_subjects.append(row)
-    return label_subjects
-
-
-def update_table(id, label, conf=1.0, table="plants"):
-    data, count = supabase_client.table(table).update(
-        {"label": label, 'confidence': conf}
-        ).eq('id', id).execute()
-    
-    return data, count
-
-
-def fetch_bucket(name, bucket="plants"):
-    try:
-        res = supabase_client.storage.from_(bucket).download(name)
-        st.image(res, caption=name, use_column_width="always")
-    except Exception as e:
-        logger.warning(e)
-
-
 def main():
     if handle_status(status):
-        st.title("Labeling")
-        st.sidebar.markdown("## Info")
+        st.title("Labelling")
 
+        # ----sidebar----
+        st.sidebar.markdown("## Info")
         conf = st.sidebar.slider('Confidence threshold', 0.0, 0.99, 0.9)
-        label_subjects = fetch_table(conf)
-        st.sidebar.markdown("###  No. of selected images")
+        label_subjects = st.session_state.supabase.fetch_table(conf)
+        st.sidebar.markdown("### No. of selected images")
         st.sidebar.write(f"{len(label_subjects)}")
+        # ---------------
         if len(label_subjects) > 0:
             _name = label_subjects[0]["name"]
             _conf = label_subjects[0]["confidence"]
-            st.sidebar.markdown("###  Current image")
+            st.sidebar.markdown("### Current image")
             st.sidebar.write(_name)
-            st.sidebar.markdown("###  Current Confidence")
+            st.sidebar.markdown("### Current Confidence")
             st.sidebar.write(_conf)
-            fetch_bucket(_name)
+            res = st.session_state.supabase.fetch_bucket(_name)
+            show_image(res, _name)
             label = st.selectbox(
                 'Label',
-                _labels,
-                index=_labels_to_ints[label_subjects[0]["label"]],
+                [k for k, _ in st.session_state.labels_to_ints.items()],
+                index=st.session_state.labels_to_ints[
+                    label_subjects[0]["label"]],
                 )
             if st.button('Next'):
-                # TODO: does not update correctly
-                update_table(id=label_subjects[0]["id"], label=label)
+                st.session_state.supabase.update_table(
+                    id=label_subjects[0]["id"], label=label)
         else:
             st.write("No saved images below the selected confidence threshold")
 
 
 if __name__ == "__main__":
-    page_config("Labeling", "👉")
-    authenticator, (name, status, user) = auth()
-    supabase_client = setup_supabase()
+    page_config("Labelling", "👉")
+    _, status, _ = auth()
     main()
